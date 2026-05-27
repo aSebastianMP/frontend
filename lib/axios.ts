76d98ea8a67f270c9e9591e20afb75b8c60ccb84
aSebastianMP <strog35777@gmail.com>
2026-05-26 23:34:36 -0600

    connect frontend to production backend

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use((config) => {
+
   const token = localStorage.getItem("token");
 
   if (token) {
